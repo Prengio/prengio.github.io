e4067318a2dea61abe8b7455b3c99575ad36bebe
Prengio <61758008+Prengio@users.noreply.github.com>
2025-06-20 17:09:45 +0000

    Deploying to gh-pages from @ Prengio/prengio.github.io@cb0153b2b9659a47998c164084bb6154ca3d75d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-books",
           title: "Books",
           description: "",
@@ -426,22 +419,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-quantum-mechanics",
-          title: 'Quantum Mechanics',
-          description: "Looking into using computation physics to model some chemistry stuff",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-machine-learning",
-          title: 'Machine Learning',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-cubic-splining-with-generating-functions",
-          title: 'Cubic splining with Generating Functions',
-          description: "The bane of a conversation with me",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+          section: "News",},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
@@ -471,40 +449,41 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-cubic-splining-with-generating-functions",
+          title: 'Cubic splining with Generating Functions',
+          description: "The bane of a conversation with me",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/GF_project/";
+            },},{id: "projects-machine-learning",
+          title: 'Machine Learning',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ML_project/";
+            },},{id: "projects-quantum-mechanics",
+          title: 'Quantum Mechanics',
+          description: "Looking into using computation physics to model some chemistry stuff",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/QM_project/";
             },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%75%6D%61%68%6D%65%64%30%37@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Prengio", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/numair-ahmed-b6557b2b7", "_blank");
         },
       },{
       id: 'light-theme',
